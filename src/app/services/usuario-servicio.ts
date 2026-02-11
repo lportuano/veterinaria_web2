59c1a3e7f62cf404538baf7f403ee00ece275bec
ramijos <amijosroy9@gmail.com>
2026-02-11 08:21:41 -0500

    Persistencia de datos

--- src/app/services/usuario-servicio.ts
+++ src/app/services/usuario-servicio.ts
@@ -10,15 +10,31 @@ export class UsuarioServicio {
 
   private http = inject(HttpClient);
 
-  private API_URL = 'https://jsonplaceholder.typicode.com/users'
+  private API_URL = 'https://698c756321a248a27361a22d.mockapi.io/usuarios'
 
+  //metodo GET
   getUsuario(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(this.API_URL)
   }
 
-  //metodo Post
+  //metodo POST
   postUsuario(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(this.API_URL, usuario);
+  }
+
+  //metodo buscar por ID
+  getUsuarioById(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.API_URL}/${id}`);
+  }
+
+  //metodo PUT
+  putUsuario(id: number, usuario: Usuario): Observable<Usuario>{
+    return this.http.put<Usuario>(`${this.API_URL}/${id}`,usuario);
+  }
+
+  //metodo DELETE
+  deleteUsuario(id:number):Observable<void>{
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
   }
 
 }
--- src/app/shared/formulario-usuario/formulario-usuario.ts
+++ src/app/shared/formulario-usuario/formulario-usuario.ts
@@ -15,6 +15,8 @@ export class FormularioUsuario {
 
   listaUsuarios = signal<Usuario[]>([]);
 
+  editando = false;
+
   nuevoUsuario: Usuario = {
     name: '',
     email: '',
@@ -33,18 +35,56 @@ export class FormularioUsuario {
     });
   }
 
-  //guardar usuarios
-  guardarUsuario(){
-    this.servicioUsuario.postUsuario(this.nuevoUsuario).subscribe(usuarioId =>{
-      //... spread operator: combina el nuevo usuario con la listausuarios
-      this.listaUsuarios.set([usuarioId, ...this.listaUsuarios()]);
-      //limpiar el form
-      this.nuevoUsuario={name:'', email:'', phone:''};
-    })
-  }
-
   mensaje() {
     alert('Gracias por registrarte');
   }
+
+  //metodo GUARDAR
+  guardarUsuario() {
+    if (this.editando && this.nuevoUsuario.id) {
+      this.servicioUsuario.putUsuario(this.nuevoUsuario.id, this.nuevoUsuario).
+        subscribe(() => {
+          this.obtener();
+          this.resetear();
+        });
+    }
+    else {
+      this.servicioUsuario.postUsuario(this.nuevoUsuario).subscribe(() => {
+        this.obtener();
+        this.resetear();
+      })
+    }
+  }
+
+  //metodo ELIMINAR
+  eliminarsuario(id: number) {
+    if (confirm('¿Desea eliminar el registro ?')) {
+      this.servicioUsuario.deleteUsuario(id).subscribe(() => {
+        this.obtener();
+      })
+    }
+  }
+
+  //metodo para poner los datos en el formulario para poder editar los inputs
+  seleccionarParaEditar(user: Usuario) {
+    this.editando = true;
+    this.nuevoUsuario = { ...user };
+  }
+
+  //metodo para limpiar los inputs (formulario)
+  resetear() {
+    this.editando = false;
+    this.nuevoUsuario = { name: '', email: '', phone: '' }
+  }
+
+  /*guardar usuarios
+  guardarUsuario() {
+    this.servicioUsuario.postUsuario(this.nuevoUsuario).subscribe(usuarioId => {
+      //... spread operator: combina el nuevo usuario con la listausuarios
+      this.listaUsuarios.set([usuarioId, ...this.listaUsuarios()]);
+      //limpiar el form
+      this.nuevoUsuario = { name: '', email: '', phone: '' };
+    })
+  }*/
 
 }
